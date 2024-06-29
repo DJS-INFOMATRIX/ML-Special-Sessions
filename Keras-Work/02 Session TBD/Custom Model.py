5f6f0bb99efddf13bda7618a9e2fc3e3a1ccfeaa
Pranav Pawar <ppawar2612@gmail.com>
2024-06-29 09:33:50 +0530

    Newer Versions

---
+++ Keras-Work/02 Session TBD/Custom Model.py
@@ -0,0 +1,20 @@
+class MyModel(keras.Model):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.conv_base = keras.Sequential(
+            [
+                keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+                keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+                keras.layers.MaxPooling2D(pool_size=(2, 2)),
+                keras.layers.Conv2D(128, kernel_size=(3, 3), activation="relu"),
+                keras.layers.Conv2D(128, kernel_size=(3, 3), activation="relu"),
+                keras.layers.GlobalAveragePooling2D(),
+            ]
+        )
+        self.dp = MyDropout(0.5)
+        self.dense = MyDense(num_classes, activation="softmax")
+
+    def call(self, x):
+        x = self.conv_base(x)
+        x = self.dp(x)
+        return self.dense(x)
